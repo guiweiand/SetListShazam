13c0cffe33c74e435228140c30f572c97d220aff
Guilherme Scapini Weiand <guilherme_weiand@hotmail.com>
2024-03-03 17:16:35 -0300

    added new index model named index2.html and added css style style.css

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 from main import run
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 @app.route("/process_url", methods=["POST"])
 def process_url():
     url = request.form["url"]
     setlist_string = run(URL=url)
-    return render_template("index.html", url=url, setlist=setlist_string)
+    return render_template("index2.html", url=url, setlist=setlist_string)
 
 if __name__ == "__main__":
     app.run(debug=True)
